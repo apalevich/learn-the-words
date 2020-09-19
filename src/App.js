7eed519c483ae3ec9f368ff1429015dd21c021d3
Artyom Palevich <hello@apalevich.ru>
2020-09-19 14:13:46 +0300

    Check out the page with cards switching by click

--- src/App.js
+++ src/App.js
@@ -1,35 +1,90 @@
 import React from 'react';
 import HeaderBlock from './components/HeaderBlock';
+import Header from './components/Header';
+import Paragraph from './components/Paragraph';
 import ContentBlock from './components/ContentBlock';
+import CardList from './components/CardList';
 import FooterBlock from './components/FooterBlock';
+import { ReactComponent as ReactLogo} from './logo.svg';
+
+const wordsList = [
+  {
+      eng: 'between',
+      rus: 'между'
+  },
+  {
+      eng: 'high',
+      rus: 'высокий'
+  },
+  {
+      eng: 'really',
+      rus: 'действительно'
+  },
+  {
+      eng: 'something',
+      rus: 'что-нибудь'
+  },
+  {
+      eng: 'most',
+      rus: 'большинство'
+  },
+  {
+      eng: 'another',
+      rus: 'другой'
+  },
+  {
+      eng: 'much',
+      rus: 'много'
+  },
+  {
+      eng: 'family',
+      rus: 'семья'
+  },
+  {
+      eng: 'own',
+      rus: 'личный'
+  },
+  {
+      eng: 'out',
+      rus: 'из/вне'
+  },
+  {
+      eng: 'leave',
+      rus: 'покидать'
+  },
+];
 
 const App = () => {
 	return (
 		<>
-			<HeaderBlock
-        title="Учите React онлайн"
-        descr = "Онлайн-марафон Зара Захарова"
+			<HeaderBlock>
+        <Header>
+          Время учить слова онлайн
+        </Header>
+        <Paragraph>
+          Используйте карточки для запоминания и пополняйте активный словарный запас
+        </Paragraph>
+      </HeaderBlock>
+      <ContentBlock
+        title="Мы создали уроки, чтобы помочь вам расширить словарный запас английских слов"
       />
       <ContentBlock
-        showLogo
-        title="Привет!"
-        descr="Это учебная страница к уроку №2 онлайн-марафона Зара Захарова. Компоненты, которые используют пропсы в качестве аргументов и собственные модули стилей. Всё вместе это собирается в файле App.js, который рендерится в точке входа — файле index.js. Ничего не забыл?"
-      />
-      <ContentBlock
-        alignText="right"
-        title="Что тут есть?"
-        descr="На странице распологаются несколько компонентов: HeaderBlock, ContentBlock и FooterBlock. Первые два из них повторяются, но выглядят по-разному благодаря пропсам."
-      />
-      <HeaderBlock
-        hideLogo
-        hideBackground
-        title="Полный список составляющих App:"
-        descr = {['Обложка HeaderBlock c пропсами по умолчанию', 'Абзац ContentBlock с включённым showLogo', 'Абзац ContentBlock c alignText равным "right"', 'HeaderBlock с включёнными hideLogo и hideBackground', 'Минималистичный FooterBlock']}
-      />
-      <FooterBlock
-        title="Спасибо за вашу работу"
-        descr="Ждём новых уроков"
-      />
+        title = "Отметьте карточки со словами, которые вам уже известны"
+        descr = "Кликните по карточке, чтобы увидеть перевод. Чтобы закрыть перевод, кликните повторно"
+        backgroundColor="#ededed"
+      >
+        <CardList item={ wordsList } />
+      </ContentBlock>
+      <HeaderBlock hideBackground>
+        <Header>
+          Изучайте английский легко и просто
+        </Header>
+        <Paragraph>
+          Уникальная технология переворачивания карточек доказала эффективность по всему миру
+        </Paragraph>
+        <ReactLogo/>
+      </HeaderBlock>
+      <FooterBlock/>
 		</>
 	);
 }
--- src/components/HeaderBlock/index.js
+++ src/components/HeaderBlock/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import s from './HeaderBlock.module.css';
-import { ReactComponent as ReactLogoSvg } from '../../logo.svg';
 
-const HeaderBlock = ({ title, hideBackground = false, hideLogo = false, descr }) => {
+const HeaderBlock = ({ hideBackground = false, children }) => {
 	const styleCover = hideBackground ? {backgroundImage: 'none '} : {};
  	return (
-		<div className={s.cover} style={styleCover }>
+		<div className={s.cover} style={ styleCover }>
 			<div className={s.wrap}>
-	 			{ title && <h1 className={s.header}>{ title }</h1> }
-				{ hideLogo || <ReactLogoSvg/> }
-	 			{ typeof descr ==  'object' ? <ul className={s.descr}>{descr.map(el => <li>{el}</li>)}</ul> : <p className={s.descr}>{ descr }</p> }
+	 			{children}
 			</div>
 		</div>
  	)
